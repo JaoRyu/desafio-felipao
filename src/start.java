c7362cd4f544d568aa0212f6ce64ead983ec4b01
JaoRyu <joao-s6@hotmail.com>
2024-07-12 11:44:43 -0300

    teceiro commit

--- src/start.java
+++ src/start.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class start {
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
@@ -5,3 +6,25 @@ public class start {
         int i = 12;
     }
 }
+=======
+import java.util.Scanner;
+
+public class start {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Digite seu nível de experiência atual: ");
+        Integer experienciaHeroi = scanner.nextInt();
+        String nomeHeroi = "Ryuzaki";
+
+        if (experienciaHeroi < 1000) {
+            System.out.println("Seu nível atual é Ferro");
+
+        }
+
+
+
+
+        }
+
+    }
+>>>>>>> 78f8a23 (terceiro commit)
