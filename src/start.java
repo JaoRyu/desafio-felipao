521ce6137b0e060023c258f1916a8247f8ca7144
JaoRyu <joao-s6@hotmail.com>
2024-07-12 14:15:43 -0300

    origin

--- src/start.java
+++ src/start.java
@@ -1,16 +1,40 @@
 import java.util.Scanner;
 public class start {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do seu herói: ");
+        String nomeHeroi = scanner.nextLine();
         System.out.println("Digite seu nível de experiência atual: ");
         Integer experienciaHeroi = scanner.nextInt();
-        String nomeHeroi = "Ryuzaki";
+        String nivelFinal = null;
+        Boolean Ferro = experienciaHeroi < 1000;
+        Boolean Bronze = experienciaHeroi > 1000 && experienciaHeroi <=2000;
+        Boolean Prata = experienciaHeroi > 2000 && experienciaHeroi <=5000;
+        Boolean Ouro = experienciaHeroi >5000 && experienciaHeroi <=7000;
+        Boolean Platina = experienciaHeroi > 7000 && experienciaHeroi <= 8000;
+        Boolean Ascendente = experienciaHeroi > 8000 && experienciaHeroi <= 9000;
+        Boolean Imortal = experienciaHeroi > 9000 && experienciaHeroi <= 10000;
+        Boolean Radiante = experienciaHeroi >= 10001;
 
-        if(experienciaHeroi < 1000) {
-            System.out.println("Seu nível atual é Ferro");
-
+        if (Ferro) {
+            nivelFinal = "Ferro";
+        } else if (Bronze) {
+            nivelFinal = "Bronze";
+        } else if (Prata) {
+            nivelFinal = "Prata";
+        } else if (Ouro) {
+            nivelFinal = "Ouro";
+        } else if (Platina) {
+            nivelFinal = "Platina";
+        } else if (Ascendente) {
+            nivelFinal = "Ascendente";
+        } else if (Imortal) {
+            nivelFinal = "Imortal";
+        } else if (Radiante) {
+            nivelFinal = "Radiante";
         }
-
+        System.out.println("O jogador " + nomeHeroi + " "+ "está no nível " + nivelFinal + ".");
         }
-
     }
+
+
